4e619cfe32a423b6bffc0a852b35388d2c824a63
nazmus767921 <nazmus767921@gmail.com>
2024-02-08 22:38:53 +0600

    v1.0

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,35 @@
+import menu from "./bx-menu.svg";
+import x from "./bx-x.svg";
+import clubs from "./clubs.svg";
+import clubsHands from "./clubsHands.svg";
+import events from "./events.svg";
+import members from "./members.svg";
+import membership from "./membership.svg";
+import nationalAssociations from "./national-associations.svg";
+import payment from "./payment.svg";
+import right from "./right.svg";
+import send from "./send.svg";
+
+// || socials
+import dribbble from "./socials/dribbble.svg";
+import instagram from "./socials/instagram.svg";
+import twitter from "./socials/twitter.svg";
+import youtube from "./socials/youtube.svg";
+
+export {
+  clubs,
+  clubsHands,
+  dribbble,
+  events,
+  instagram,
+  members,
+  membership,
+  menu,
+  nationalAssociations,
+  payment,
+  right,
+  send,
+  twitter,
+  x,
+  youtube,
+};
